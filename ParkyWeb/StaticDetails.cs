46205f7f02443002757e6e47e6150e0811905b44
Elif <elifozer173@gmail.com>
2021-08-05 14:55:18 +0300

    Initialized ParkyWeb MVC project Added StaticDetails class Added Razor.RuntimeCompilation NuGet package

---
+++ ParkyWeb/StaticDetails.cs
@@ -0,0 +1,9 @@
+namespace ParkyWeb
+{
+    public static class StaticDetails
+    {
+        public static string APIBaseURL = "http://localhost:5000/";
+        public static string NationalParkAPIPath = APIBaseURL + "api/v1/nationalparks";
+        public static string TrailAPIPath = APIBaseURL + "api/v1/trails";
+    }
+}
