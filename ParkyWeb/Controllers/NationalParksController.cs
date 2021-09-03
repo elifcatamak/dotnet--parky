686d393c59ce81e59949a0cacbc1d5ada3673759
Elif <elifozer173@gmail.com>
2021-09-03 10:58:57 +0300

    Upsert post action method implemented

--- ParkyWeb/Controllers/NationalParksController.cs
+++ ParkyWeb/Controllers/NationalParksController.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ParkyWeb.Models;
@@ -46,6 +47,57 @@ namespace ParkyWeb.Controllers
             }
 
             return View(nationalPark);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(NationalPark nationalPark)
+        {
+            if (ModelState.IsValid)
+            {
+                // Image related
+                var files = HttpContext.Request.Form.Files;
+
+                if (files.Count > 0) // If image is uploaded
+                {
+                    byte[] p1 = null;
+
+                    using (var fileStream1 = files[0].OpenReadStream()) // This will read the file
+                    {
+                        using (var memoryStream1 = new MemoryStream())
+                        {
+                            fileStream1.CopyTo(memoryStream1);
+                            p1 = memoryStream1.ToArray();
+                        }
+                    }
+
+                    nationalPark.Image = p1;
+                }
+                else // If image is not uploaded
+                {
+                    if (nationalPark.Id != 0)
+                    {
+                        var dataFromDb =
+                            await _npRepository.GetAsync(StaticDetails.NationalParkAPIPath, nationalPark.Id);
+                        nationalPark.Image = dataFromDb.Image;
+                    }
+                }
+
+                if (nationalPark.Id == 0) // Create
+                {
+                    await _npRepository.CreateAsync(StaticDetails.NationalParkAPIPath, nationalPark);
+                }
+                else // Update
+                {
+                    await _npRepository.UpdateAsync(StaticDetails.NationalParkAPIPath + nationalPark.Id, nationalPark);
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(nationalPark);
+            }
         }
     }
 }
